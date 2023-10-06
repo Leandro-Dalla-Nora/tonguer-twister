a1a97d651c2cf20072bb357ca5fad1f5e238c1ef
Leandro Dalla Nora <leandro.nora@acad.ufsm.br>
2023-10-06 11:04:05 -0300

    Refatoração da função que realiza a contagem das letras mais repetidas

--- src/utilities.py
+++ src/utilities.py
@@ -4,21 +4,16 @@ import pandas as pd
 from collections import Counter
 
 
-def read_file():
+def read_file() -> list[str]:
     return pd.read_csv('./dataset/database.csv')["Tongue twister"].tolist()
 
 
-def counter_words(db) -> list[int]:
+def counter_words(db: list[str]) -> list[int]:
     return [len(frase.split()) for frase in db]
 
 
-def repetition_letters(db) -> list[list[tuple[str, int]]]:
-    lista = []
-
-    for frase in db:
-        lista.append(Counter(frase.lower().replace(' ', '')).most_common(2))
-
-    return lista
+def repetition_letters(db: list[str]) -> list[list[tuple[str, int]]]:
+    return [Counter(frase.lower().replace(' ', '')).most_common(2) for frase in db]
 
 
 
